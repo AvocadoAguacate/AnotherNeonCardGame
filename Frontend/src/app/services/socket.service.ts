b2fdeb3175268df9ba9140166130b9a0f98ebeba
AvocadoAguacate <estebanguzra@gmail.com>
2024-10-04 08:48:30 -0600

    UPDATE socket service

--- Frontend/src/app/services/socket.service.ts
+++ Frontend/src/app/services/socket.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Socket, SocketIoConfig } from 'ngx-socket-io';
+import { card } from '../interfaces/card.models';
 
 @Injectable({
   providedIn: 'root'
@@ -7,24 +8,39 @@ import { Socket, SocketIoConfig } from 'ngx-socket-io';
 export class SocketService  {
 
   constructor(private socket: Socket) {
+    
   }
-  getMessage(){
-    this.socket.on('hand', (msg: any) => {
-      console.log(msg);
+  public hand: card[] = [];
+  getHand(){
+    this.socket.on('hand', (msg: card[]) => {
+      this.hand = msg;
     });
   }
-  send() {
-    this.socket.emit('chat message', {test: 'soy un test', id: this.socket.ioSocket.id});
+
+  setReady(status: boolean) {
+    this.send('ready', {playerId: this.socket.ioSocket.id, status});
   }
+
+  setPlayer(name:string, picIndex:number){
+    this.send('setPlayer', {id: this.socket.ioSocket.id, picIndex, name});
+  }
+
+  send(chanel:string, msg:any) {
+    this.socket.emit(chanel, msg);
+  }
+  
   tryConnect(){
     this.socket.connect((res) => {
       console.log(res);
     });
   }
 
-  updateSocketUrl(newUrl: string) {
+  updateSocketUrl(newUrl: string, name: string) {
     this.socket.disconnect();
     this.socket.ioSocket.io.uri = newUrl;
-    this.socket.connect();
+    this.socket.connect(res => {
+      console.log(res);
+    });
+    this.socket.emit('chat message', {name, id: this.socket.ioSocket.id});
   }
 }
