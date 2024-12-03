ca73d0c81faf3536e2c67286f66bd3bb7c9640cd
AvocadoAguacate <estebanguzra@gmail.com>
2024-12-02 18:24:39 -0600

    TS tranform index

---
+++ Backend/src/index.ts
@@ -0,0 +1,39 @@
+import { Message } from './interfaces/message.model';
+import express from 'express'
+import { createServer } from 'node:http';
+import { Server, Socket } from 'socket.io';
+import { Game } from './game';
+
+
+const app = express()
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+
+let sockets: Socket[] = [];
+let game: Game = new Game();
+
+io.on('connection', (socket) => {
+  sockets.push(socket);
+  console.log(`Acaba de conectarse ${socket.id}`);
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg: Message) => {
+    console.log(`${msg.id} quiere ejecutar ${msg.type}`);
+    const socket = sockets.find(s => s.id === msg.id);
+    if (socket){
+      game.execute(msg);
+    }
+  })
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
