d48ae5c9a6199a26d8789f1424d4f01315003070
AvocadoAguacate <estebanguzra@gmail.com>
2024-12-13 23:11:40 -0600

    ADD card prototype

--- Frontend/src/app/components/card/card.component.ts
+++ Frontend/src/app/components/card/card.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardUI } from './../../interfaces/update.model';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  constructor(){
+  }
+
+  @Input()
+  public card!: CardUI
+  @Input()
+  public isChain!: boolean
+  ngOnInit(): void {
+    if(!this.card) throw new Error('The card is required');
+  }
+  private isChainNumber(): boolean{
+    if(this.card.number! >= 14 && this.card.number! <= 35) return true;
+    return false;
+  }
+
+  getSrc():string {
+    return `assets/Cards/c${this.card.number!}.svg`;
+  }
 
 }
