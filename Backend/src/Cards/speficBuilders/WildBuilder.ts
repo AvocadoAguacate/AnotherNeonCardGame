bb837c90e319879fd7301b1a97a7fa7443062b3b
AvocadoAguacate <estebanguzra@gmail.com>
2024-12-22 14:54:55 -0600

    ADD null case  in wilds

--- Backend/src/Cards/speficBuilders/WildBuilder.ts
+++ Backend/src/Cards/speficBuilders/WildBuilder.ts
@@ -1,9 +1,13 @@
-import { Card, PlayPayload } from "../../interfaces/card.model";
+import { Card, Color, PlayPayload } from "../../interfaces/card.model";
 import { Context } from "../../interfaces/context.model";
 import { addFunction } from "../CardBuilder";
 
 function playWild(context: Context, payload?:PlayPayload):Context {
   let {discardDeck} = context;
+  if(payload?.wildColor){
+    let col: Color[] = ['blue', 'red', 'purple', 'yellow', 'green'];
+    payload.wildColor = col[Math.floor(Math.random() * 5)];
+  }
   discardDeck[0].colors = [payload!.wildColor!];
   return {...context, discardDeck};
 }
