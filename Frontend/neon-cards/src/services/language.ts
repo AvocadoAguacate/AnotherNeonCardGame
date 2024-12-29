45e4c098e7951c136f858e1dd02386b86c3a3365
AvocadoAguacate <estebanguzra@gmail.com>
2024-12-28 22:01:57 -0600

    ADD language service

---
+++ Frontend/neon-cards/src/services/language.ts
@@ -0,0 +1,45 @@
+export class LanguageService {
+  private currentLanguage: string; 
+  private translations: Record<string, Record<string, string>>;
+
+  constructor(){
+    this.currentLanguage = 'es';
+    this.translations = {
+      en: {
+        IP_SERVER: "...",
+        waitingRoom: "Waiting Room",
+        gameOver: "Game Over",
+        welcome: "Welcome to the Game",
+        play: "Play",
+        exit: "Exit",
+      },
+      es: {
+        IP_SERVER: "Ingresa el ip del server",
+        waitingRoom: "Sala de Espera",
+        gameOver: "Fin del Juego",
+        welcome: "Bienvenido al Juego",
+        play: "Jugar",
+        exit: "Salir",
+      },
+    };
+  }
+
+  setLanguage(language: string) {
+    if (this.translations[language]) {
+      this.currentLanguage = language;
+    } else {
+      console.warn(`Idioma no soportado: ${language}`);
+    }
+  }
+
+  translate(key: string): string {
+    return (
+      this.translations[this.currentLanguage][key] ||
+      `Traducción no encontrada: ${key}`
+    );
+  }
+
+  getLanguage(): string {
+    return this.currentLanguage;
+  }
+}
