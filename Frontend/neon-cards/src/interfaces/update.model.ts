eaf88eccdc5cacbf9f8752db14a6711d46cd9906
AvocadoAguacate <estebanguzra@gmail.com>
2024-12-30 13:17:08 -0600

    ADD interfaces

---
+++ Frontend/neon-cards/src/interfaces/update.model.ts
@@ -0,0 +1,35 @@
+import { Color } from "./message.model"
+
+
+export type TypeUI = 'updateUI' | 'challenge' | 'voteDeck' 
+
+export interface messageUI{
+  type: TypeUI
+}
+
+export interface UpdateUI extends messageUI{
+  lastDiscard  ?: CardUI,
+  hand         ?: CardUI[],
+  players      ?: PlayerUI[],
+  deadNumber   ?: number,
+  turn         ?: number,
+  chain        ?: number,
+  general      ?: number[]    
+}
+
+export interface CardUI{
+  number  : number,
+  colors  : Color[],
+  id      : string
+}
+
+export interface PlayerUI{
+  name    : string,
+  hand    : number,
+  img     : number
+}
+
+export interface ChallengeUI extends messageUI{
+  oponent : number,
+  id      : string
+}
