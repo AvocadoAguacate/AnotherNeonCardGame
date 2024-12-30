bf115147b7248484e7186210749bb0deced8a414
AvocadoAguacate <estebanguzra@gmail.com>
2024-12-30 13:14:00 -0600

    UPDATE gameservice

--- Frontend/neon-cards/src/services/game.ts
+++ Frontend/neon-cards/src/services/game.ts
@@ -1,5 +1,21 @@
-export class GameService {
-  constructor(){
+import { EditPlayerMessage } from "../interfaces/message.model";
 
+export class GameService {
+  private picInd: number
+  private name: string 
+  constructor(){
+    this.picInd = -1;
+    this.name = "";
+  }
+
+  public getEditPlayerMsg(name: string, pic: number):EditPlayerMessage{
+    return {
+      id: "",
+      type: 'editPlayer',
+      payload: {
+        name,
+        img: pic
+      }
+    }
   }
 }
