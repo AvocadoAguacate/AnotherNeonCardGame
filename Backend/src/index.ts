6606f6394569af7f5be7bcdd0035115d874cd945
AvocadoAguacate <estebanguzra@gmail.com>
2024-12-29 19:05:24 -0600

    UPDATE CORS

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -9,7 +9,7 @@ const app = express()
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:4200",
+    origin: "http://localhost:8080",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
