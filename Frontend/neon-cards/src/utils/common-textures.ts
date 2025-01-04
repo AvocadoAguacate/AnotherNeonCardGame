3e6a8abaa24c37c7621d969e5f1aef6b4a1f090d
AvocadoAguacate <estebanguzra@gmail.com>
2025-01-03 20:32:55 -0600

    ADD more types of cards

---
+++ Frontend/neon-cards/src/utils/common-textures.ts
@@ -0,0 +1,7 @@
+import { Assets } from "pixi.js";
+
+export const chainTexture = await Assets.load(`/assets/Cards/chain.svg`);
+export const generalTexture = await Assets.load(`/assets/Cards/general.svg`); 
+export const allTexture = await Assets.load(`/assets/Cards/all.svg`);
+export const randomTexture = await Assets.load(`/assets/Cards/random.svg`); 
+export const oneTexture = await Assets.load(`/assets/Cards/one.svg`); 
