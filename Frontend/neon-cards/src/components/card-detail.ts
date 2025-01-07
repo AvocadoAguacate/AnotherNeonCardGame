beb0720573879f961de333fdf927738466373af7
AvocadoAguacate <estebanguzra@gmail.com>
2025-01-06 19:22:58 -0600

    ADD card-detail

---
+++ Frontend/neon-cards/src/components/card-detail.ts
@@ -0,0 +1,49 @@
+import { Container, Text } from 'pixi.js';
+import { LanguageService } from '../services/language';
+import { CardUI } from './../interfaces/update.model';
+import { createCard } from './card';
+import { ScrollBox } from '@pixi/ui';
+export async function createCardDetail(
+  card:CardUI, lang:LanguageService, size:number[]
+):Promise<Container>{
+  const detail = new Container({
+    height: 300,
+    width: 600,
+  });  
+  const title = new Text({
+    text: lang.translate(`c${card.number}TITLE`),
+    style:{
+      fontFamily:'Orbitron',
+      fill: 0xd1d7ff,
+      align: 'center',
+      wordWrap: true,
+      wordWrapWidth: size[0] * 2,
+      fontSize:18,
+      fontWeight: 'bolder'
+    }
+  });
+  const textDetail = new Text({
+    text: lang.translate(`c${card.number}DETAIL`),
+    style:{
+      fontFamily:'Orbitron',
+      fill: 0xd1d7ff,
+      align: 'justify',
+      wordWrap: true,
+      wordWrapWidth: size[0] * 2,
+      fontSize:16
+    },
+  });
+  const textContainer = new ScrollBox({
+    height: size[1],
+    width: size[0] * 2,
+    items: [title, textDetail],
+    // background: 0xffffff
+  });
+  title.x = size[0] - title.width /2;
+  textContainer.position.set(size[0] * 1.125, 0);
+  textContainer.label = `card-detail-text-${card.number}`
+  detail.addChild(textContainer);
+  const cardContainer = await createCard(card, size);
+  detail.addChild(cardContainer);
+  return detail;
+}
