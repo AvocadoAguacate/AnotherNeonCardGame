cc3743449678f078f9b31830935301f1169cdeb8
AvocadoAguacate <estebanguzra@gmail.com>
2025-01-09 21:52:57 -0600

    ADD my subject

---
+++ Frontend/neon-cards/src/components/observable.ts
@@ -0,0 +1,24 @@
+type Listener<T> = (value: T) => void;
+
+class MySubject<T> {
+  private listeners: Listener<T>[] = [];
+  private currentValue: T;
+
+  constructor(initialValue: T) {
+    this.currentValue = initialValue;
+  }
+
+  get value(): T {
+    return this.currentValue;
+  }
+
+  subscribe(listener: Listener<T>): void {
+    this.listeners.push(listener);
+    listener(this.currentValue);
+  }
+
+  next(value: T): void {
+    this.currentValue = value;
+    this.listeners.forEach(listener => listener(value));
+  }
+}
