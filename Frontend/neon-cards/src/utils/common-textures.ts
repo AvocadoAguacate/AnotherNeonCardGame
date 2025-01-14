14efd8117ddb109e8024d671d38e7dd1dc45688f
AvocadoAguacate <estebanguzra@gmail.com>
2025-01-13 20:48:56 -0600

    UPDATE common-textures

--- Frontend/neon-cards/src/utils/common-textures.ts
+++ Frontend/neon-cards/src/utils/common-textures.ts
@@ -8,3 +8,9 @@ export const oneTexture = await Assets.load(`/assets/Cards/one.svg`);
 
 export const likeTexture = await Assets.load(`/assets/Buttons/thumb-up.svg`); 
 export const disLikeTexture = await Assets.load(`/assets/Buttons/thumb-down.svg`); 
+export const colorPickTexture = await Assets.load(`/assets/Buttons/pickColor.svg`); 
+export const pickNumberTexture = await Assets.load(`/assets/Buttons/pickNumber.svg`); 
+export const selectDiscardsTexture = await Assets.load(`/assets/Buttons/selectDiscards.svg`); 
+export const drawTexture = await Assets.load(`/assets/Buttons/cardDraw.svg`); 
+export const helpTexture = await Assets.load(`/assets/Buttons/help.svg`); 
+export const passTexture = await Assets.load(`/assets/Buttons/pass.svg`); 
